f650a5a1d13f3e5e2c027dcb0d1553c2d5a9c12b
yomannino <yomannino@gmail.com>
2021-04-08 09:24:34 -0400

    new commit more debugging

--- api.php
+++ api.php
@@ -2,56 +2,56 @@
 $servername ="localhost";
  $username = "root";
   $password = "changeme";
-  $dbname = "sqlheros";
+  $dbname = "SQLHeros2";
   
   //create connection
-  $conn= new mysqli($servername, $username, $password, $dbname):
+  $conn= new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("connection failed:" .$conn->)
+    die("connection failed:" .$conn->connect_error);
 }
-
-$myData = getHeroByID($conn, 5);
-echo $myData;
-
-$conn->close();
-
 function createBattle ($conn, $h1, $h2, $w) {
-  $sql = "INSERT INTO battles (hero1, hero2, winner)"
+  $sql = "INSERT INTO battles (hero1, hero2, winner)
   VALUES ($h1, $h2, $w)";
-  $record
+//   $record
 }
-
-function getAllheroes (){
+function getAllHeroes ($conn){
   $data=array();
-  $sql = "SELECT * FROM heros";
+  $sql = "SELECT * FROM heroes";
   $result = $conn->query($sql);
   
-  if ($result)->num_rows > 0) {
+  if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()){
-      $data .="id:" . $row["id"]. "- Name: " . $row{''}
+      ///$data .="id:" . $row["id"]. "- Name: " . $row{''}
       
       array_push($data,$row);
     }
   }
+  return json_encode($data);
 }
 
 function getHeroByID ($conn, $heroID){
    $data= array();
-  $sql = "SELECT * FROM heros";
-  $result = $conn_>quey($sql); 
+  $sql = "SELECT * FROM heroes";
+  $result = $conn->quey($sql); 
 }
 
+$route = $_GET['route'];
+print_r($_GET);
+
 switch ($route) {
-  case "getALLheros":
-  $myData = getHeroByID($conn, $id);
+  case "getAllHeroes":
+    echo "line 49";
+    $myData = getAllHeroes($conn);
     break;
   case "getHeroByID":
-    $id = $_GET['hero_id']:
+    $id = $_GET['hero_id'];
   case "createBattle"
-    $myData = ()
+    $myData = create_function($conn);
     break;
   default:
-    $myData = json_encode([])
+        $myData = json_encode([])
 }
+echo $myData;
+$conn->close();
 ?>
